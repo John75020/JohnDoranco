5529bd222aea968e265871e7d2844a7124d54426
John75020 <67926616+John75020@users.noreply.github.com>
2020-07-08 15:52:29 +0200

    quatrieme commit

--- quatriemecommit.js
+++ quatriemecommit.js
@@ -1,6 +1,17 @@
+var N = 15;
+for(var i=15; i<30;i++)
+	if(i % 2 === 0){
+		console.log(i)
+	}
+}
+
+
+ou ainsi...
+
 function Odd(value) {
     return (value & 1)==1;
 }
 for(var I=5;I<=15;I++) {
     console.log("Le nombre "+I+" est "+(Odd(I)?"Impaire":"Pair"));
 }
+
